f34d15e3ef723583af9ab65a2a6c16afe25e2271
XXMKYX <mikael.angelo.txt@gmail.com>
2023-05-31 17:46:33 -0600

    Get Data from DB
    Informacion traida del contenido de las tablas de la DB

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,19 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostController extends AbstractController
 {
-    #[Route('/post', name: 'app_post')]
-    public function index(): Response
+    #[Route('/post/{id}', name: 'app_post')]
+    public function index(Post $post): Response
     {
         return $this->render('post/index.html.twig', [
             'controller_name' => 'PostController',
             'user_name' => 'Miguel Angel',
             'cadena' => ['Value1'=> 'Primer valor','Value2'=> 'Segundo valor'],
+            'post' => $post,
         ]);
     }
 }
